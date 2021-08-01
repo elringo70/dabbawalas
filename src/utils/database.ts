fc8657fe2aa48eaf22ee334db64152423ff6f0e4
Luis Alvarez <elringo70@hotmail.com>
2021-07-31 21:46:57 -0500

    database

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,17 +1,22 @@
 import mysql from 'mysql'
 
 const poolConfig = {
-    host: 'localhost',
-    user: 'root',
-    database: 'dabbawalas',
-    password: ''
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'b1c97ef4c685ed',
+    database: 'heroku_95d44d52299a0eb',
+    password: 'fb8b618a'
 }
 
 export const pool = mysql.createPool(poolConfig)
 
 export const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'dabbawalas',
-    password: ''
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'b1c97ef4c685ed',
+    database: 'heroku_95d44d52299a0eb',
+    password: 'fb8b618a'
 })
+
+/* host: 'localhost',
+user: 'root',
+database: 'dabbawalas',
+password: '' */
